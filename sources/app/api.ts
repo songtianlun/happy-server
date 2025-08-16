35b4ce412c3c8e02c86d1adae9deadf408dbead0
Steve Korshakov <steve@korshakov.com>
2025-08-16 10:13:44 -0700

    feat: add app-to-app authentication

--- sources/app/api.ts
+++ sources/app/api.ts
@@ -240,6 +240,80 @@ export async function startApi(): Promise<{ app: FastifyInstance; io: Server }> 
         return reply.send({ success: true });
     });
 
+    // Account auth request
+    typed.post('/v1/auth/account/request', {
+        schema: {
+            body: z.object({
+                publicKey: z.string(),
+            }),
+            response: {
+                200: z.union([z.object({
+                    state: z.literal('requested'),
+                }), z.object({
+                    state: z.literal('authorized'),
+                    token: z.string(),
+                    response: z.string()
+                })]),
+                401: z.object({
+                    error: z.literal('Invalid public key')
+                })
+            }
+        }
+    }, async (request, reply) => {
+        const publicKey = privacyKit.decodeBase64(request.body.publicKey);
+        const isValid = tweetnacl.box.publicKeyLength === publicKey.length;
+        if (!isValid) {
+            return reply.code(401).send({ error: 'Invalid public key' });
+        }
+
+        const answer = await db.accountAuthRequest.upsert({
+            where: { publicKey: privacyKit.encodeHex(publicKey) },
+            update: {},
+            create: { publicKey: privacyKit.encodeHex(publicKey) }
+        });
+
+        if (answer.response && answer.responseAccountId) {
+            const token = await tokenGenerator.new({ user: answer.responseAccountId! });
+            return reply.send({
+                state: 'authorized',
+                token: token,
+                response: answer.response
+            });
+        }
+
+        return reply.send({ state: 'requested' });
+    });
+
+    // Approve account auth request
+    typed.post('/v1/auth/account/response', {
+        preHandler: app.authenticate,
+        schema: {
+            body: z.object({
+                response: z.string(),
+                publicKey: z.string()
+            })
+        }
+    }, async (request, reply) => {
+        const publicKey = privacyKit.decodeBase64(request.body.publicKey);
+        const isValid = tweetnacl.box.publicKeyLength === publicKey.length;
+        if (!isValid) {
+            return reply.code(401).send({ error: 'Invalid public key' });
+        }
+        const authRequest = await db.accountAuthRequest.findUnique({
+            where: { publicKey: privacyKit.encodeHex(publicKey) }
+        });
+        if (!authRequest) {
+            return reply.code(404).send({ error: 'Request not found' });
+        }
+        if (!authRequest.response) {
+            await db.accountAuthRequest.update({
+                where: { id: authRequest.id },
+                data: { response: request.body.response, responseAccountId: request.user.id }
+            });
+        }
+        return reply.send({ success: true });
+    });
+
     // Sessions API
     typed.get('/v1/sessions', {
         preHandler: app.authenticate,
