86c0a03bfc8ff5f1c44be3d41cd19bba4ce54c98
Steve Korshakov <steve@korshakov.com>
2025-08-26 21:56:03 -0700

    fix: fix image

--- sources/storage/processImage.spec.ts
+++ sources/storage/processImage.spec.ts
@@ -1,5 +1,7 @@
 import * as fs from 'fs';
 import { processImage } from './processImage';
+import { describe, it } from 'vitest';
+
 describe('processImage', () => {
     it('should resize image', async () => {
         let img = fs.readFileSync(__dirname + '/__testdata__/image.jpg');
